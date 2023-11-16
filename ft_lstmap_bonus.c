0305684df5f3b7791bbffccda55ed84e2eb64d59
TulipeSoyeuse <romain.dupeux24@gmail.com>
2023-11-16 13:43:01 +0100

    debug norm

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,46 +6,33 @@
 /*   By: rdupeux <rdupeux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:52:09 by rdupeux           #+#    #+#             */
-/*   Updated: 2023/11/16 13:24:51 by rdupeux          ###   ########.fr       */
+/*   Updated: 2023/11/16 13:36:39 by rdupeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	check(t_list *lst, void *(*f)(void *), void (*del)(void *),
-		t_list *res)
-{
-	if (!lst || !f || !del)
-		return (NULL);
-	res = ft_lstnew((*f)(lst->content));
-	if (!res)
-		return (NULL);
-}
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*current_res;
-	t_list	*res;
-	t_list	*current;
-	t_list	*next;
+	t_list	*new_list;
+	t_list	*new;
 	void	*set;
 
-	if (check(lst, f, del, res))
-		return ((NULL)current = lst->next);
-	while (current)
+	if (!lst || !f || !del)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
 	{
-		set = (*f)(current->content);
-		current_res = ft_lstnew(set);
-		if (!current_res)
+		set = f(lst->content);
+		new = ft_lstnew(set);
+		if (!new)
 		{
 			del(set);
-			ft_lstclear(&res, *del);
-			return (res);
+			ft_lstclear(&new_list, *del);
+			return (new_list);
 		}
-		ft_lstadd_back(&res, current_res);
-		next = current->next;
-		current = next;
+		ft_lstadd_back(&new_list, new);
+		lst = lst->next;
 	}
-	ft_lstlast(res)->next = NULL;
-	return (res);
+	return (new_list);
 }
