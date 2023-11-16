98e6aba7359dcc655eb5ba5d8cd9f81fa29eb6e7
Romain Dupeux <rdupeux@made-f0Cr10s5.clusters.42paris.fr>
2023-11-16 13:25:53 +0100

    norm

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,11 +6,21 @@
 /*   By: rdupeux <rdupeux@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:52:09 by rdupeux           #+#    #+#             */
-/*   Updated: 2023/11/15 14:00:37 by rdupeux          ###   ########.fr       */
+/*   Updated: 2023/11/16 13:24:51 by rdupeux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	check(t_list *lst, void *(*f)(void *), void (*del)(void *),
+		t_list *res)
+{
+	if (!lst || !f || !del)
+		return (NULL);
+	res = ft_lstnew((*f)(lst->content));
+	if (!res)
+		return (NULL);
+}
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -20,12 +30,8 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*next;
 	void	*set;
 
-	if (!lst || !f || !del)
-		return (NULL);
-	res = ft_lstnew((*f)(lst->content));
-	if (!res)
-		return (NULL);
-	current = lst->next;
+	if (check(lst, f, del, res))
+		return ((NULL)current = lst->next);
 	while (current)
 	{
 		set = (*f)(current->content);
@@ -43,30 +49,3 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	ft_lstlast(res)->next = NULL;
 	return (res);
 }
-
-// void *		lstmap_f(void *content) {
-// 	(void)content;
-// 	return ("OK !");
-// }
-
-// int				__delNum = 0;
-// void			lstdel_f(void *lst) {
-// 	(void)lst;
-// 	__delNum++;
-// }
-
-// int	main(void)
-// {
-// 	t_list *l = ft_lstnew(strdup(" 1 2 3 "));
-// 	t_list *ret;
-
-// 	l->next = ft_lstnew(strdup("ss"));
-// 	l->next->next = ft_lstnew(strdup("-_-"));
-// 	ret = ft_lstmap(l, lstmap_f, lstdel_f);
-// 	printf("%d\n",strcmp(ret->content, "OK !"));
-// 	printf("%d\n",strcmp(ret->next->content, "OK !"));
-// 	printf("%d\n",strcmp(ret->next->next->content, "OK !"));
-// 	printf("%d\n",strcmp(l->content, " 1 2 3 "));
-// 	printf("%d\n",strcmp(l->next->content, "ss"));
-// 	printf("%d\n",strcmp(l->next->next->content, "-_-"));
-// }
